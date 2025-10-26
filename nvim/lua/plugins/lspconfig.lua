04298ba063a13db4ca904631d15f8878169f0486
Neil Viloria <neilcviloria@gmail.com>
2025-10-25 22:29:40 -0600

    lspconfig: run clangd in docker container

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -30,4 +30,23 @@ return {
       },
     },
   },
+  {
+    "mason.nvim",
+    {
+      "mason-org/mason-lspconfig.nvim",
+      config = function()
+        local lspconfig = require("lspconfig")
+
+        -- #NOTE only for the tcp/ip stack project
+        lspconfig.clangd.setup({
+          cmd = { "docker", "exec", "-i", "tcpdev", "clangd", "--background-index" },
+          root_dir = lspconfig.util.root_pattern("compile_commands.json", ".git", "."),
+          -- capabilities = require("cmp_nvim_lsp").default_capabilities(),
+          -- on_attach = on_attach,
+          -- settings = servers[server_name],
+          -- filetypes = (servers[server_name] or {}).filetypes,
+        })
+      end,
+    },
+  },
 }
