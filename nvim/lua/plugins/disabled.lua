aaf85534f2b4525a0c261c29200ac4e8eb8ee14e
Neil Viloria <neilcviloria@gmail.com>
2025-10-26 12:32:18 -0600

    disabled: snacks

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -1,4 +1,16 @@
 return {
   { "folke/trouble.nvim", enabled = false },
   { "folke/flash.nvim", enabled = false },
+  {
+    "folke/snacks.nvim",
+    opts = {
+      explorer = { enabled = false },
+    },
+    keys = {
+      { "<leader>fe", false },
+      { "<leader>fE", false },
+      { "<leader>E", false },
+      { "<leader>e", false },
+    },
+  },
 }
