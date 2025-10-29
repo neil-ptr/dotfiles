eca8ec3f9223037b5d1c88fbab6dddd73f1c335c
Neil Viloria <neilcviloria@gmail.com>
2025-10-28 20:50:54 -0600

    neotree: copy file to clipboard bind

--- nvim/lua/plugins/neotree.lua
+++ nvim/lua/plugins/neotree.lua
@@ -69,14 +69,44 @@ return {
           ["l"] = "open",
           ["h"] = "close_node",
           ["<space>"] = "none",
-          ["Y"] = {
-            function(state)
-              local node = state.tree:get_node()
-              local path = node:get_id()
-              vim.fn.setreg("+", path, "c")
-            end,
-            desc = "Copy Path to Clipboard",
-          },
+          ["Y"] = function(state)
+            -- NeoTree is based on [NuiTree](https://github.com/MunifTanjim/nui.nvim/tree/main/lua/nui/tree)
+            -- The node is based on [NuiNode](https://github.com/MunifTanjim/nui.nvim/tree/main/lua/nui/tree#nuitreenode)
+            local node = state.tree:get_node()
+            local filepath = node:get_id()
+            local filename = node.name
+            local modify = vim.fn.fnamemodify
+
+            local results = {
+              filepath,
+              modify(filepath, ":."),
+              modify(filepath, ":~"),
+              filename,
+              modify(filename, ":r"),
+              modify(filename, ":e"),
+            }
+
+            -- absolute path to clipboard
+            local i = vim.fn.inputlist({
+              "Choose to copy to clipboard:",
+              "1. Absolute path: " .. results[1],
+              "2. Path relative to CWD: " .. results[2],
+              "3. Path relative to HOME: " .. results[3],
+              "4. Filename: " .. results[4],
+              "5. Filename without extension: " .. results[5],
+              "6. Extension of the filename: " .. results[6],
+              "\n",
+            })
+
+            if i > 0 then
+              local result = results[i]
+              if not result then
+                return print("Invalid choice: " .. i)
+              end
+              vim.fn.setreg("+", result)
+              vim.notify("Copied: " .. result)
+            end
+          end,
           ["O"] = {
             function(state)
               require("lazy.util").open(state.tree:get_node().path, { system = true })
