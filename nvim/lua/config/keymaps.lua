89752c05002a16f63047f6d7621a43b8efb49a1b
Neil Viloria <neilcviloria@gmail.com>
2025-10-30 12:46:21 -0600

    keymaps: comments

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,11 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
 vim.api.nvim_set_keymap("n", "zz", "zz:noh<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<C-u>", "<C-u>zz", { noremap = true })
 vim.api.nvim_set_keymap("n", "<C-d>", "<C-d>zz", { noremap = true })
 
--- https://github.com/LazyVim/LazyVim/discussions/658
+-- Fix lazyvim moving lines up or down: https://github.com/LazyVim/LazyVim/discussions/658
 vim.api.nvim_set_keymap("n", "<A-k>", "<esc>k", { desc = "Move up", noremap = true })
 vim.api.nvim_set_keymap("n", "<A-j>", "<esc>j", { desc = "Move down", noremap = true })
 vim.api.nvim_set_keymap("i", "<A-k>", "<esc>gk", { desc = "Move up", noremap = true })
