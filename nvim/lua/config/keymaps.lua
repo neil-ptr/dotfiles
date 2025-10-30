613baaf494beacfbd721ebeb109ed860d3577969
Neil Viloria <neilcviloria@gmail.com>
2025-10-30 12:43:33 -0600

    keymaps: fix move lines bug

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -4,3 +4,11 @@
 vim.api.nvim_set_keymap("n", "zz", "zz:noh<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<C-u>", "<C-u>zz", { noremap = true })
 vim.api.nvim_set_keymap("n", "<C-d>", "<C-d>zz", { noremap = true })
+
+-- https://github.com/LazyVim/LazyVim/discussions/658
+vim.api.nvim_set_keymap("n", "<A-k>", "<esc>k", { desc = "Move up", noremap = true })
+vim.api.nvim_set_keymap("n", "<A-j>", "<esc>j", { desc = "Move down", noremap = true })
+vim.api.nvim_set_keymap("i", "<A-k>", "<esc>gk", { desc = "Move up", noremap = true })
+vim.api.nvim_set_keymap("i", "<A-j>", "<esc>gj", { desc = "Move down", noremap = true })
+vim.api.nvim_set_keymap("v", "<A-k>", "<esc>gk", { desc = "Move up", noremap = true })
+vim.api.nvim_set_keymap("v", "<A-j>", "<esc>gj", { desc = "Move down", noremap = true })
